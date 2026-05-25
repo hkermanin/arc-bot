07a4aca1d0f573ba49e5b7bbb2bc320a232130a5
kermani <97584991+hkermanin@users.noreply.github.com>
2026-05-25 14:32:06 +0000

    Setup dotenvy environment loading

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod db;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenvy::dotenv().ok();
+    
     pretty_env_logger::init();
 
     let db = db::init_db().await?;
